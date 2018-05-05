2d2710fcdd342d1923c8ec85b7293bba0e79974b
José Díaz <jadm333@gmail.com>
2018-05-05 07:57:32 -0500

    4ta sesion

--- 4_sesion/ejercicio.R
+++ 4_sesion/ejercicio.R
@@ -41,3 +41,22 @@ creditos = Altas_clientes %>% group_by(Cliente) %>% summarise(num = n())
 
 # 6
 
+df2 = df %>% mutate(month = format(`Fecha de alta` , "%m"), year = format(`Fecha de alta`, "%Y")) %>% group_by(month, year) %>% summarise(total = sum(Monto), maxi = max(Monto), mini = min(Monto), prom = mean(Monto))
+
+##########
+Altas_clientes2 = Altas_clientes %>% separate(Contrato, into = c("Nsuc", "Contrato"), sep = "-", convert = TRUE) %>% left_join(Diccionario_sucursales)
+
+# 1
+porSuc = Altas_clientes2 %>% mutate(mes = format(`Fecha de alta` , "%m")) %>% select(Nsuc, Monto, mes) %>% group_by(Nsuc,mes) %>% summarise(total=sum(Monto)) %>% spread(key = mes,value = total)
+
+MONTOTOTAL = sum(Altas_clientes2$Monto)
+
+
+###########
+#1
+cortes <- c(-Inf, 1, 29, 59, 89, Inf)
+niveles <- 1:5
+
+hist_de_cobranza2 = hist_de_cobranza %>% mutate(MOP = cut(`Días de mora`, breaks = cortes, labels = niveles, right = FALSE) )
+
+
